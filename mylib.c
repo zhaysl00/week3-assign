d276e375f4259ee03ff25672ffc9d1daf548dd1c
Zachary Hayslip <zhaysl00@uafs.edu>
2026-09-06 19:03:51 -0500

    Week 3

--- mylib.c
+++ mylib.c
@@ -1,12 +1,88 @@
 #include "mylib.h"
+#include <stddef.h>
 
-int cstrlen(char *str){
-    int len=0;
+int cstrlen(char *str)
+{
+    int len = 0;
 
-    while(*str != '\0'){
+    while (*str != '\0')
+    {
         len++;
         str++;
     }
 
     return len;
+}
+
+char *cstrcpy(char *dest, char *src)
+{
+    char *p = dest;
+
+    while (*src != '\0')
+    {
+        *p = *src;
+        p++;
+        src++;
+    }
+
+    *p = '\0';
+
+    return p;
+}
+
+char *cstrcat(char *dest, char *src)
+{
+    char *p = dest;
+
+    while (*p != '\0')
+    {
+        p++;
+    }
+
+    while (*src != '\0')
+    {
+        *p = *src;
+        p++;
+        src++;
+    }
+
+    *p = '\0';
+
+    return dest;
+}
+
+char *cstrchr(char *str, int c)
+{
+    while (*str != '\0')
+    {
+        if (*str == c)
+        {
+            return str;
+        }
+
+        str++;
+    }
+
+    if (c == '\0')
+    {
+        return str;
+    }
+
+    return NULL;
+}
+
+int cstrcmp(char *s1, char *s2)
+{
+    while (*s1 != '\0' && *s2 != '\0')
+    {
+        if (*s1 != *s2)
+        {
+            return (unsigned char)*s1 - (unsigned char)*s2;
+        }
+
+        s1++;
+        s2++;
+    }
+
+    return (unsigned char)*s1 - (unsigned char)*s2;
 }
--- mylib.h
+++ mylib.h
@@ -1,2 +1,7 @@
 // ** Function Prototypes
+
 int cstrlen(char *str);
+char *cstrcpy(char *dest, char *src);
+char *cstrcat(char *dest, char *src);
+char *cstrchr(char *str, int c);
+int cstrcmp(char *s1, char *s2);
